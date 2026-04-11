09050c96eb35979e3570df1e140fa91eccf00a31
Abdul Kader <kader.bd.developer@gmail.com>
2026-04-11 09:49:26 +0600

    Hope

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
-dotenv.config("./.env");
+dotenv.config({ path: "./.env" });
 
 connectDB()
   .then(() => {
     const PORT = process.env.PORT || 4000;
-    app.listen(PORT, '0.0.0.0', () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on http://0.0.0.0:${PORT}`);
       console.log(`Health check: http://localhost:${PORT}/health`);
     });
